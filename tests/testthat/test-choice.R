7ce8d40b48ef83cf7c2c0d6441d17e9afd8f285e
Patrick Lloyd-Smith <plloydsmith@gmail.com>
2019-02-03 17:59:48 -0600

    Add basic tests

---
+++ tests/testthat/test-choice.R
@@ -0,0 +1,12 @@
+context("First test")
+
+#data(eggs, package = "rmdcev")
+
+test_that("basic test", {
+    # If the number below needs to be increased due to additional outputs,
+    # ensure that the output size does not get too big when there are multiple
+    # classes and many iterations.
+    expect_true(1 < 1245000)
+    # Add the following option to expect_true to print out the size in Travis
+    # info = print(as.numeric(object.size(result))))
+})
