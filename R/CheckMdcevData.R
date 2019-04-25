b9abe6dd3390688903d60723c8caf47a62d60d09
Patrick <plloydsmith@gmail.com>
2019-04-25 11:35:49 -0600

    add arrange check on id/good

--- R/CheckMdcevData.R
+++ R/CheckMdcevData.R
@@ -31,10 +31,19 @@ CheckMdcevData <- function(data){
 		print(paste0("Numeraire is less than 0 for individuals in rows: ",
 					 toString(as.character(which(check$numeraire < 0)))))
 		stop()
-}
+	}
+
+	check <- tbl_df(data) %>%
+		arrange(id, good)
+
+	if (identical(identical(check$id, data$id), FALSE) || identical(identical(check$good, data$good), FALSE) ){
+		stop("Data must be arranged by id then good. Can use dplyr command arrange(id, good).")
+	}
+
 	if (sum(data$price <= 0) > 0){
 		print(paste0("Price is less than or equal to 0 for at least one individual good in rows: ",
 					 toString(as.character(which(data$price <= 0)))))
-}
+		stop()
+	}
 	message("Data is good")
 }
