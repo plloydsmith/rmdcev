43f7e9825bb4444406c1e01e8cf653121e4f8af8
Patrick Lloyd-Smith <plloydsmith@gmail.com>
2020-04-08 16:14:59 -0600

    update startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,4 @@
 .onAttach <- function(...) {
 	ver <- utils::packageVersion("rmdcev")
 	packageStartupMessage("This is rmdcev version ", ver)
-	packageStartupMessage("- This package continues to be under development so may change")
 }
